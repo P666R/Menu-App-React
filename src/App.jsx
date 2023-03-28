75ba6a926ccc4db7f849398bc3cc672669b2515a
PRoy <sam469roger@gmail.com>
2023-03-28 20:49:35 +0530

    implented filtering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import Categories from "./components/Categories";
+import Menu from "./components/Menu";
+import Title from "./components/Title";
+import menu from "./data";
+
+const allCategories = ["all", ...new Set(menu.map((el) => el.category))];
+
 function App() {
-  return <div>app</div>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCatogories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    console.log(category);
+  };
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="menu app" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
